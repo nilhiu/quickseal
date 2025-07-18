16b7e02a719635dcafd4a5dc20b507863bbabbd7
nilhiu <heresy@disroot.org>
2025-07-18 20:52:20 +0400

    Move all Flask app config and creation to `create_app` function.

--- api/app.py
+++ api/app.py
@@ -4,15 +4,22 @@ from routes import routes
 from db import db
 from models import Model
 
-app = Flask(__name__)
-# TODO: Change the DB implementation to Postgres.
-app.config["SQLALCHEMY_ENGINES"] = {"default": "sqlite:///api.db"}
-# TODO: Make UPLOAD_PATH customizable via an env var.
-app.config["UPLOAD_PATH"] = "data/"
-db.init_app(app)
-alembic = Alembic(app, metadatas=Model.metadata)
+
+alembic = Alembic(metadatas=Model.metadata)
+
+
+def create_app() -> Flask:
+    app = Flask(__name__)
+    # TODO: Change the DB implementation to Postgres.
+    app.config["SQLALCHEMY_ENGINES"] = {"default": "sqlite:///api.db"}
+    # TODO: Make UPLOAD_PATH customizable via an env var.
+    app.config["UPLOAD_PATH"] = "data/"
+    db.init_app(app)
+    alembic.init_app(app)
+    app.register_blueprint(routes)
+    return app
 
 
 if __name__ == "__main__":
-    app.register_blueprint(routes)
+    app = create_app()
     app.run(host="0.0.0.0", port=8081, debug=True)
