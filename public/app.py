062224adfa0aa44f4eecd72e3d308ab5c5770b66
nilhiu <heresy@disroot.org>
2025-07-20 16:04:36 +0400

    Add broadcast shares page.

--- public/app.py
+++ public/app.py
@@ -9,5 +9,10 @@ def index():
     return render_template("index.j2")
 
 
+@app.route("/broadcast")
+def broadcast():
+    return render_template("broadcast.j2", shares=[{"id": "some_id"}])
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
