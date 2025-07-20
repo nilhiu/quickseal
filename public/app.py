741f8413f7fe931cd4a5ef75590dc2257fbc1b2b
nilhiu <heresy@disroot.org>
2025-07-20 20:44:32 +0400

    Add file share page.

--- public/app.py
+++ public/app.py
@@ -14,5 +14,10 @@ def broadcast():
     return render_template("broadcast.j2", shares=[{"id": "some_id"}])
 
 
+@app.route("/broadcast/<int:share_id>")
+def files(share_id: int):
+    return render_template("files.j2", share_id=share_id, files=["file.txt"])
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
